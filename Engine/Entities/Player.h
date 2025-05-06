44ecbf50ff7f9075245126e7a5e82cf186ef6842
seajay39673 <chris.easy15@gmail.com>
2025-05-06 13:01:56 -0600

    feat: toggle gravity and collisions

--- Engine/Entities/Player.h
+++ Engine/Entities/Player.h
@@ -1,15 +1,11 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
-#include <chrono>
-
 #include "../Camera.h"
 #include "../Physics/Gravity.h"
 #include "../Chunk/ChunkManager.h"
 
 using Engine::ChunkSpace::ChunkManager;
-
-using namespace std::chrono;
 
 namespace Engine::Entities
 {
@@ -23,11 +19,22 @@ namespace Engine::Entities
         Player(Camera &cam) : velocity(0), camera(cam) {}
         void Update(float dt)
         {
-            vec3 currentPos = camera.GetCameraPos();
-            vec3 offset = ChunkManager::HandleCollisions(currentPos, GetMin(), GetMax());
-            vec3 newPos = camera.GetCameraPos() + offset;
-            vec3 gravityOffset = Physics::Gravity::Step(velocity, dt);
-            newPos += gravityOffset;
+            vec3 newPos = camera.GetCameraPos();
+            vec3 offset(0);
+            vec3 gravityOffset(0);
+
+            if (collisionsEnabled)
+            {
+                offset = ChunkManager::HandleCollisions(newPos, GetMin(), GetMax());
+                newPos += offset;
+            }
+
+            if (gravityEnabled)
+            {
+                vec3 gravityOffset = Physics::Gravity::Step(velocity, dt);
+                newPos += gravityOffset;
+            }
+
             if (offset.y != 0)
                 velocity.y = 0;
             if (newPos.y < 0)
@@ -36,11 +43,42 @@ namespace Engine::Entities
         }
         Camera &GetCamera() { return camera; }
         vec3 &GetPos() { return camera.GetCameraPos(); }
-        void ProcessInput() { camera.ProcessInput(); }
+        void ProcessInput()
+        {
+            if (Keyboard::keys[GLFW_KEY_G])
+            {
+                if (!gravityKeyPressed)
+                {
+                    gravityKeyPressed = true;
+                    toggleGravity();
+                }
+            }
+            else
+                gravityKeyPressed = false;
+            if (Keyboard::keys[GLFW_KEY_C])
+            {
+                if (!collisionKeyPressed)
+                {
+                    collisionKeyPressed = true;
+                    toggleCollisions();
+                }
+            }
+            else
+                collisionKeyPressed = false;
+            camera.ProcessInput();
+        }
         vec3 GetMin() { return camera.GetCameraPos() - vec3(.4f, 1.8, .4f); }
         vec3 GetMax() { return camera.GetCameraPos() + vec3(.4f, 0, .4f); }
 
     private:
+        void toggleGravity() { gravityEnabled = !gravityEnabled; }
+        void toggleCollisions() { collisionsEnabled = !collisionsEnabled; }
+
+        bool collisionKeyPressed = false;
+        bool collisionsEnabled = true;
+
+        bool gravityKeyPressed = false;
+        bool gravityEnabled = true;
         Camera camera;
         vec3 velocity;
     };
